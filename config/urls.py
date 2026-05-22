bd3dab8f1173e8a98fb06951b9ea9f652d2944c1
Gongzuo-Dk <gongzuo.dk@gmail.com>
2026-05-22 13:42:53 +0300

    Add Google OAuth configuration with allauth and dj-rest-auth

--- config/urls.py
+++ config/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('auth/social/', include('dj_rest_auth.registration.urls')),
+    path('accounts/', include('allauth.urls')),
     path('api/', include('expenses.urls')),
+    path('auth/google/', include('accounts.urls')),
 ]
